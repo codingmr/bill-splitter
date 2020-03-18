9cf21fb0c56bbe174e453a4b6ab2676029817f22
M Roberts <memroberts@debian>
2020-03-18 03:40:28 +0000

    Bare tab navigation

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabBarIcon from '../components/TabBarIcon';
+import HelpScreen from '../screens/HelpScreen';
+import HomeScreen from '../screens/HomeScreen';
+import HistoryScreen from '../screens/HistoryScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Help"
+        component={HelpScreen}
+        options={{
+          title: 'Help',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-help-circle" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Bill Splitter',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-cash" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          title: 'History',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+        }}
+      />
+    <BottomTab.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{
+        title: 'Settings',
+        tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-settings" />,
+      }}
+    />
+  </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Help':
+      return 'Bill will help';
+    case 'Home':
+      return "Get splittin'";
+    case 'History':
+      return 'Find a split bill';
+    case 'Settings':
+      return 'Change it up';
+  }
+}
