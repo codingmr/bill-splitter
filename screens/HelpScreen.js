2cd1d163c8085a8bb61f21c39dc14b5662b83d21
codingmr <codingmroberts@gmail.com>
2020-04-15 15:31:24 +0100

    Helpscreen notes

--- screens/HelpScreen.js
+++ screens/HelpScreen.js
@@ -6,9 +6,32 @@ import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
 export default function HelpScreen() {
   return (
-    <View style={styles.centerMsg}>
-      <Text>[This screen is intentionally blank]</Text>
-      <Text>Updates coming soon..</Text>
+    <View style={}>
+
+      <Text>1. Creating a new group</Text>
+      // render create group button
+      // draw arrow pointing to button
+
+
+      <Text>2. Group header</Text>
+      // render an empty group with one person
+      // draw arrow pointing to delete button
+
+      // I want to
+      //  - Create a new group
+      //  - Delete a group
+      //  - Edit a group name
+      //  - Input item prices
+      //  - Change an item category
+      //  - Change the number of people
+      //  - Add a tip
+      //  - Know how the bill is split
+      //  - Know the sum total of all the groups
+
+      //  - Split the bill evenly
+      //  - Split the bill unevenly
+      //  -  
+
     </View>
   );
 }
